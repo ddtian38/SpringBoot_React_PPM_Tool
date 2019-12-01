20c60a4aa1dc316599fb847ce0f82f9ffe1325a1
Daniel Tian <danieltian31@yahoo.com>
2019-11-30 20:18:52 -0600

    add redux

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,20 +2,24 @@ import React from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import Header from './components/Layout/Header';
 import AddProject from './components/Project/AddProject';
-
+import {Provider} from 'react-redux';
+import store from "./store"
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header/>
-        <Route  path="/dashboard" component={Dashboard}/>
-        <Route exact path="/addProject" component={AddProject}/>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header/>
+          <Route  path="/dashboard" component={Dashboard}/>
+          <Route exact path="/addProject" component={AddProject}/>
+        </div>
+      </Router>
+   </Provider>
+
 
   );
 }
--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -1,13 +1,87 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {createProject} from "../../actions/projectActions"
+import { create } from 'domain';
 
 class AddProject extends Component{
+    constructor(){
+        super();
+
+        this.state ={
+            "projectName": "",
+            "projectIdentifer": "",
+            "description": "",
+            "startDate": "",
+            "endDate": ""
+        }
+
+        this.onChange=this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+    }
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        
+        const newProject = {
+            "projectName": this.state.projectName,
+            "projectIdentifer": this.state.projectIdentifer,
+            "description": this.state.description,
+            "startDate": this.state.startDate,
+            "endDate": this.state.endDate
+        }
+
+        this.props.createProject(newProject, this.props.history);
+
+    }
+
     render(){
         return (
-            <div>
-                <h1>Add Project Form</h1>
+            <div className="register">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 m-auto">
+                        <h5 className="display-4 text-center">Create Project form</h5>
+                        <hr />
+                        <form onSubmit={this.onSubmit}>
+                            <div className="form-group">
+                                <input type="text" className="form-control form-control-lg " placeholder="Project Name" name="projectName" value={this.state.projectName} onChange={this.onChange}/>
+                            </div>
+                            <div className="form-group">
+                                <input type="text" className="form-control form-control-lg" placeholder="Unique Project ID" name="projectIdentifer" value={this.state.projectIdentifer} onChange={this.onChange}
+                                     />
+                            </div>
+                            <div className="form-group">
+                                <textarea className="form-control form-control-lg" placeholder="Project Description" name="description" value={this.state.description} onChange={this.onChange.bind(this)}></textarea>
+                            </div>
+                            <h6>Start Date</h6>
+                            <div className="form-group">
+                                <input type="date" className="form-control form-control-lg" name="startDate" value={this.state.startDate} onChange={this.onChange}/>
+                            </div>
+                            <h6>Estimated End Date</h6>
+                            <div className="form-group">
+                                <input type="date" className="form-control form-control-lg" name="endDate" value={this.state.endDate} onChange={this.onChange}/>
+                            </div>
+    
+                            <input type="submit" className="btn btn-primary btn-block mt-4"/>
+                        </form>
+                    </div>
+                </div>
             </div>
+        </div>
         )
     }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+    createProject: PropTypes.func.isRequired
+}
+
+export default connect(null, {createProject}) (AddProject);
